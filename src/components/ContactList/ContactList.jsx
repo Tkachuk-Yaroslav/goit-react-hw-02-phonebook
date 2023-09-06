5d5383fd5a71ce71bcad5eb179a0a5b268d5cb55
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-07 01:47:38 +0300

    2nd task

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,37 @@
-// import css from './ContactList.module.css';
-import { Component } from 'react';
+import css from './ContactList.module.css';
+// import { Component } from 'react';
 
-class ContactList extends Component {
-  state = {
-    contacts: [],
-    name: '',
-  };
+// class ContactList extends Component {
+//   state = {
+//     contacts: [],
+//     name: '',
+//   };
 
-  render() {
-    return <h3>hello</h3>;
-  }
-}
+//   render() {
+//     return <h3>hello</h3>;
+//   }
+// }
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id} className={css.item}>
+            <h2 className={css.title}>{contact.name}</h2>
+            <h2 className={css.number}>{contact.number}</h2>
+            <button
+              className={css.delete}
+              onClick={() => onDeleteContact(contact.id)}
+              type="button"
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ContactList;
